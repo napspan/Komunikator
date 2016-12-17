f50e464737437cfa8cfc43a105ba8de8de62a462
napspan <napspan@o2.pl>
2016-12-17 19:46:54 +0100

    Dodanie 3 aktywności i metody onThreActivity

--- app/src/main/java/com/napspan/komunikator/CreateMessageActivity.java
+++ app/src/main/java/com/napspan/komunikator/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -14,7 +15,16 @@ public class CreateMessageActivity extends Activity {
     }
 
     public void onSendMessage(View view) {
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
     }
+
+    public void onThreActivity(View view){
+        Intent intent = new Intent(this, activity3.class);
+        startActivity(intent);
+    }
+
 }
